f2aa32fae8cd8c0cb8e9fc76b6e9651d0e1c1e19
bilalrasool123 <47560440+bilalrasool123@users.noreply.github.com>
2025-01-13 16:40:26 +0500

    z index of cookies accept reject

--- src/components/CookieBanner.jsx
+++ src/components/CookieBanner.jsx
@@ -24,7 +24,7 @@ const CookieBanner = () => {
   if (!isBannerVisible) return null;
 
   return (
-    <div className="fixed bottom-0 w-[100%] left-1/2 transform -translate-x-1/2 bg-gray-800 text-white p-16">
+    <div className="fixed bottom-0 w-[100%] left-1/2 transform -translate-x-1/2 bg-gray-800 text-white p-16 z-[1000]">
       <p className="flex justify-center">We use cookies to improve your experience. Accept or reject?</p>
       <div className="flex justify-center mt-4 gap-4">
         <button
